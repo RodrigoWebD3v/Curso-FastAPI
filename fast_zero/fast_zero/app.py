a38dbb389b3c40cc92b60e0b69f09e614f20fffb
Rodrigo Cardoso <rodrigo.cawebdev@gmail.com>
2026-06-12 13:36:20 -0300

    Criado schemas

--- fast_zero/fast_zero/app.py
+++ fast_zero/fast_zero/app.py
@@ -1,11 +1,14 @@
 from fastapi import FastAPI
 
+from http import HttpStatus
+
+from fast_zero.schemas import Message
+
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code=HttpStatus.OK, response_model=Message)
 def read_root():
-    return {'message': 'Hello, World!'}
-
+    return {'message': 'Olá Mundo!'}
 
 print(read_root())
